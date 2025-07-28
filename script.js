f8c084663ddec546d095c44b821cd4593092e3fc
Aimee3104 <ammamymyatmon@gmail.com>
2025-07-27 20:30:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById('yesBtn').addEventListener('click', () => {
     <h1>Great! 🎉</h1>
     <p>📍 <strong>30 Gordon Street, Allston, MA 02134</strong></p>
     <p>🕕 <strong>1st August at 6 PM</strong></p>
+    <p>Fill out this form to RSVP:</p>
+    <iframe src="https://docs.google.com/forms/d/e/YOUR_FORM_ID/viewform?embedded=true" width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   `;
 });
 
@@ -10,6 +12,6 @@ document.getElementById('noBtn').addEventListener('click', () => {
   document.body.style.backgroundColor = 'black';
   document.body.style.color = 'white';
   document.getElementById('container').innerHTML = `
-    <div style="font-size: 5em;">🔴 🔺 ◻️</div>
+    <div id="logo">🔴 △ □</div>
   `;
 });
